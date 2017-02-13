1eb3d85b96618c086cae8344ea5aaada21035cc1
Imran Khan <m_imran_khan@me.com>
2017-02-14 00:04:40 +0100

    styling

--- js/greeting.js
+++ js/greeting.js
@@ -1,14 +1,53 @@
+// add daybreak
 var today = new Date();
-var hourNow = today.getHours();
+var hour = today.getHours();
+var greeting;
 
-if (hourNow >= 18) {
-	var greeting = "Good evening!";
-} else if (hourNow >= 12) {
-	var greeting = "Good afternoon!";
-} else if (hourNow >= 0) {
-	var greeting = "Good morning!";
-} else {
-	var greeting = "Welcome!";
+if (hour >= 18) {	
+	greeting = "Good evening!";
+}else if (hour >= 12) {
+	greeting = "Good afternoon!";
+}else if (hour >= 0) {
+	greeting = "Good morning!";
+}else {
+	greeting = "Welcome!";
+}
+	
+document.write('<center>' + '<h2 class="greeting" style=\"font-size:60px; font-family:Brush Script MT", cursive;\">' + greeting + '</h2>' + '</center>');
+
+// Timeclock
+function startTime() {
+var today = new Date();
+    
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    var ampm = isAMorPM(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    document.getElementById('time').innerHTML = 'The current time is' + " " + '<i>'+ h + ":" + m + ":" + s  + " - " + ampm + '</i>';
+    setTimeout(startTime, 1000);
 }
 
-document.write('<center>' + '<h2 class ="greeting">' + greeting + '</h2>' + '</center>');
+function isAMorPM(i) {
+	var val;
+	if (i >= 12) {
+		val = "PM"
+	} else {
+	 	val = "AM"
+}
+return val;
+}
+
+// add zero in front
+function checkTime(i) {
+	if (i < 10) {i = "0" + i};
+	return i;
+}
+
+
+
+
+
+
+
